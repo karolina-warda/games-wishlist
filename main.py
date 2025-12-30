db38b632108a40514e97018813b8bc44fefd2d3a
Karolina Warda <karolimera@gmail.com>
2025-12-30 11:34:02 +0100

    adding export to csv

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import pandas as pd
 
-element_list = []
+def get_element(url):
+    try: 
+        element = driver.find_element(By.XPATH, url).text
+    except NoSuchElementException: 
+        element = 'N/A'
+    return element
+
+def cast_float(price):
+    try: 
+        price_float = float(price.replace("zl", "").replace(",", "."))
+    except ValueError:
+        price_float = 0.00
+    return price_float
+
 
 # Set up Chrome options (optional)
 options = webdriver.ChromeOptions()
@@ -19,65 +33,62 @@ service = Service(ChromeDriverManager().install())
 
 # Initialize driver properly
 driver = webdriver.Chrome(service=service, options=options)
+
+games_data = []
+
 #game_name = "coral island"
-game_names = ["coral island", "pentiment", "Coral islaAd", "firewatch", "soma"]
-# with open("./games_list", "r") as file:
-#     game_names = [line.strip() for line in file]
-#     print(game_names)
+#game_names = ["coral island", "The walking dead"]
+with open("./games_list", "r") as file:
+    game_names = [line.strip() for line in file]
+    print(game_names)
 for game_name in game_names:
     search_url = f"https://store.playstation.com/en-pl/search/"+game_name 
     driver.get(search_url)
     game_url = driver.find_element(By.XPATH, '//div[@data-qa-index="0"]//div[@data-qa="search#productTile0"]//a[@data-qa=""]').get_attribute("href")
-    print(game_url)
+    #print(game_url)
     driver.get(game_url)
     time.sleep(2)
-    game_title = driver.find_element(By.XPATH, '//h1[@data-qa="mfe-game-title#name"]').text
+    game_title = get_element('//h1[@data-qa="mfe-game-title#name"]')
 
-    try: 
-        current_price = driver.find_element(By.XPATH, '//span[@data-qa="mfeCtaMain#offer0#finalPrice"]').text
-    except NoSuchElementException: 
-        current_price = 'N/A'
-
-    try: 
-        final_price = driver.find_element(By.XPATH, '//span[@data-qa="mfeCtaMain#offer1#finalPrice"]').text
-    except NoSuchElementException: 
-        final_price = 'N/A'
-
-    try: 
-        normal_price = driver.find_element(By.XPATH, '//span[@data-qa="mfeCtaMain#offer0#originalPrice"]').text
-    except NoSuchElementException: 
-        normal_price = 'N/A'
+    current_price = get_element('//span[@data-qa="mfeCtaMain#offer0#finalPrice"]')
+    final_price = get_element('//span[@data-qa="mfeCtaMain#offer1#finalPrice"]')
+    normal_price = get_element('//span[@data-qa="mfeCtaMain#offer0#originalPrice"]')
 
     if current_price == 'Included':
         is_included = 'Yes'
     else: 
         is_included = 'No'
 
-    try: 
-        current_price_float = float(current_price.replace("zl", "").replace(",", "."))
-    except ValueError:
-        current_price_float = 0.00
-
-    try: 
-        final_price_float = float(final_price.replace("zl", "").replace(",", "."))
-    except ValueError:
-        final_price_float = 0.00
-
-    try: 
-        normal_price_float = float(normal_price.replace("zl", "").replace(",", "."))
-    except ValueError:
-        normal_price_float = 0.00
+    current_price_float = cast_float(current_price)
+    final_price_float = cast_float(final_price)
+    normal_price_float = cast_float(normal_price)
 
     prices = sorted([current_price_float, final_price_float, normal_price_float])
 
     if prices[1] != 0.00:
-        discount = round(((prices[1]/max(prices)) * 100), 0)
+        discount = 100 - round(((prices[1]/max(prices)) * 100), 0)
     else: 
         discount = 0
-
     
-    print(game_title, is_included, discount, current_price, final_price, normal_price)
+    if get_element('//span[@data-qa="mfeCtaMain#offer0#discountDescriptor"]') == 'N/A':
+        discount_expiration = get_element('//span[@data-qa="mfeCtaMain#offer1#discountDescriptor"]')
+    else:
+        discount_expiration = get_element('//span[@data-qa="mfeCtaMain#offer0#discountDescriptor"]')
+    
+    game_dict = {"serached_title":game_name,
+                "game_title":game_title, 
+                 "is_included":is_included, 
+                 "discount":discount, 
+                 "discount_expiration":discount_expiration, 
+                 "current_price":current_price,
+                 "final_price":final_price,
+                 "normal_price":normal_price,
+                 "game_url":game_url}
+    
+    games_data.append(game_dict)
+    #print(game_title, is_included, discount, discount_expiration, current_price, final_price, normal_price)
 
-# TODO: add discount expiration date
+df=pd.json_normalize(games_data)
+df.to_csv('games_data.csv', encoding='utf-8', index=False)
 driver.quit()
 
